c7df355de51e95ab80dc9fc3d4b877e241b42ecf
Li Changcheng <18307110235@fudan.edu.cn>
2023-05-03 16:21:04 +0800

    ??

--- example.go
+++ example.go
@@ -6,6 +6,6 @@ import "math/rand"
 func ReadNumber() int {
 	// random number range
 	rnr := 10
-	//  returns, as an int, a non-negative pseudo-random number in [0,n)
 	return rand.Intn(rnr)
+	//
 }
